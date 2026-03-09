05d4318812eaf7c2ec69f8951d919aa3d29ef454
MOHAMEDASHIKM <mmdashik955@gmail.com>
2026-03-09 10:12:46 +0530

    aline the code

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 function Display(value) {
-    document.getElementById('display').value += value;
-  }
-  
-  function Clear() {
-    document.getElementById('display').value = '';
-  }
-  
-  function Result() {
-    var result = eval(
+  document.getElementById('display').value += value;
+}
+
+function Clear() {
+  document.getElementById('display').value = '';
+}
+
+function Result() {
+  var result = eval(
     document.getElementById('display').value
-    );
-    document.getElementById('display').value = result;
-  }
+  );
+  document.getElementById('display').value = result;
+}
 
 
-  
